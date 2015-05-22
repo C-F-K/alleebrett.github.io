693d971ef1726293663354ec7e80f561f9e96d8e
Allee Brett <alleebrett@gmail.com>
2015-05-22 17:06:19 +0200

    allee

--- src/showResponse.jsx
+++
@@ -1,9 +0,0 @@
-function showResponse (response) {
-            RESPONSE = response;
-            if (this && this.url && (typeof(this.url) == "string")) {
-                var anchor = jQuery("#url");
-                anchor.text(this.url.toString());
-                anchor.attr('href', this.url.toString());
-            }
-            jQuery("#output").text(JSON.stringify(response, null, '  '));
-        }
